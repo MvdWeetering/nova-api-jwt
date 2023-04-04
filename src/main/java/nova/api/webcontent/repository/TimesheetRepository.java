dd24138ab3e0a0952e3c2aa782afb8d32b4ac6e9
MichelvandeWeetering <michel.van.de.weetering@gmail.com>
2023-04-04 11:46:21 +0200

    fixed code block

--- src/main/java/nova/api/webcontent/repository/TimesheetRepository.java
+++ src/main/java/nova/api/webcontent/repository/TimesheetRepository.java
@@ -8,12 +8,12 @@ import java.util.List;
 public interface TimesheetRepository extends JpaRepository<timesheetByCampaign, Long> {
 
     @Query(value = """
-SELECT tbl_timesheet.id, tbl_campaign.campaign_name, tbl_mission.mission_name, tbl_user.UserName, tbl_timesheet.time_total 
-FROM csar.tbl_user INNER JOIN (((csar.tbl_campaign INNER JOIN csar.tbl_mission ON csar.tbl_campaign.Id = csar.tbl_mission.fk_campaign_id) 
-INNER JOIN csar.tbl_entity ON csar.tbl_mission.Id = csar.tbl_entity.fk_mission_id) 
-INNER JOIN csar.tbl_timesheet ON (csar.tbl_entity.Id = csar.tbl_timesheet.fk_entity_id) 
-AND (csar.tbl_entity.Id = csar.tbl_timesheet.fk_entity_id)) ON (csar.tbl_user.Id = tbl_timesheet.fk_user_id) 
-AND (csar.tbl_user.Id = csar.tbl_timesheet.fk_user_id) WHERE (((tbl_campaign.campaign_name)= :query )) """, nativeQuery=true)
+SELECT tbl_timesheet.id, tbl_campaign.campaign_name, tbl_mission.mission_name, tbl_user.UserName, tbl_timesheet.time_total
+FROM csar.tbl_user INNER JOIN (((csar.tbl_campaign INNER JOIN csar.tbl_mission ON csar.tbl_campaign.Id = csar.tbl_mission.fk_campaign_id)
+INNER JOIN csar.tbl_entity ON csar.tbl_mission.Id = csar.tbl_entity.fk_mission_id)
+INNER JOIN csar.tbl_timesheet ON (csar.tbl_entity.Id = csar.tbl_timesheet.fk_entity_id)
+AND (csar.tbl_entity.Id = csar.tbl_timesheet.fk_entity_id)) ON (csar.tbl_user.Id = tbl_timesheet.fk_user_id)
+AND (csar.tbl_user.Id = csar.tbl_timesheet.fk_user_id) WHERE (((tbl_campaign.campaign_name)= :query ))""", nativeQuery=true)
 
     List<timesheetByCampaign> returnTimesheet(String query);
 
